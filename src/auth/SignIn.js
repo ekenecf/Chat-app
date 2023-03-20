6a42dba24efddd2fc5856c0e7996bfe378632a45
ekenecf <ekens2u@gmail.com>
2023-03-20 18:55:01 +0100

    Add transition to signIn component

--- src/auth/SignIn.js
+++ src/auth/SignIn.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
 function SignIn() {
+  AOS.init({
+    duration: 1700,
+  })
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="px-8 py-6 mt-4 text-left bg-white shadow-lg">
-        <h3 className="text-2xl font-bold text-center">
+    <div className="flex items-center justify-center min-h-screen bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-gray-900 via-gray-100 to-gray-900">
+      <div className="px-8 py-6 mt-4 text-left bg-white shadow-lg md:w-2/4 ">
+        <h3 className="text-2xl font-bold text-center" data-aos="fade-right">
           Login to your account
         </h3>
-        <form className="">
+        <form className="" data-aos="fade-in">
           <div className="mt-4">
             <div>
               <label className="block" htmlFor="email">
@@ -28,14 +34,24 @@ function SignIn() {
                 className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
               />
             </div>
+            <div className="mt-4">
+              <label className="block">Confirm Password</label>
+              <input
+                type="password"
+                placeholder="Password"
+                className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+              />
+            </div>
             <div className="flex items-baseline justify-between">
               <button className="px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900">
                 Login
               </button>
-              {/* <a href="#" className="text-sm text-blue-600 hover:underline">
+              <Link
+                to="/sign-in"
+                className="text-sm text-blue-600 hover:underline"
+              >
                 Forgot password?
-              </a> */}
-            <Link to="/sign-in" className="text-sm text-blue-600 hover:underline">Forgot password?</Link>
+              </Link>
             </div>
           </div>
         </form>
--- src/auth/SignUp.js
+++ src/auth/SignUp.js
@@ -9,95 +9,94 @@ const SignUp = () => {
     duration: 1700,
   })
   return (
-    <div className="w-full flex flex-col items-center bg-gradient-to-t from-zinc-300 to-zinc-50 md:flex md:flex-row md:h-screen md:pr-40 md:pl-20 md:pb-8">
+    <div className="w-full flex flex-col items-center bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-indigo-200 via-slate-600 to-indigo-200 md:flex md:flex-row md:h-screen md:pr-40 md:pl-20 md:pb-8">
       <div className="w-3/5" data-aos="fade-in" data-aos-delay="50">
         <img src={Image} alt="LogInImge" />
       </div>
-      <form class="w-full max-w-lg pl-11 pr-11 pt-8">
-        <div class="flex flex-wrap -mx-3 mb-5">
-          <div class="w-full md:w-1/2 px-3 mb-5 md:mb-0" data-aos="fade-right">
+      <form className="w-full max-w-lg pl-11 pr-11 pt-8">
+        <div className="flex flex-wrap -mx-3 mb-5">
+          <div
+            className="w-full md:w-1/2 px-3 mb-5 md:mb-0"
+            data-aos="fade-right"
+          >
             <label
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              for="grid-first-name"
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-first-name"
             >
               First Name
             </label>
             <input
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="grid-first-name"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
               type="text"
               placeholder="Jane"
             />
-            <p class="text-red-500 text-xs italic" data-aos="fade-in">
+            <p className="text-red-500 text-xs italic" data-aos="fade-in">
               Please fill out this field.
             </p>
           </div>
-          <div class="w-full md:w-1/2 px-3" data-aos="fade-left">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 border-red-500">
+          <div className="w-full md:w-1/2 px-3" data-aos="fade-left">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 border-red-500">
               Last Name
             </label>
             <input
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-last-name"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               type="text"
               placeholder="Doe"
             />
           </div>
         </div>
-        <div class="flex flex-wrap -mx-3 mb-5">
-          <div class="w-full px-3" >
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+        <div className="flex flex-wrap -mx-3 mb-5">
+          <div className="w-full px-3">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
               Password
             </label>
             <input
-              class="appearance-none block w-full bg-gray-200  border-red-500 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-password"
+              className="appearance-none block w-full bg-gray-200  border-red-500 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               type="password"
               placeholder="******************"
             />
-            <p class="text-red-500 text-xs italic mb-4" data-aos="fade-in">
+            <p className="text-red-500 text-xs italic mb-4" data-aos="fade-in">
               Please fill out this field.
             </p>
           </div>
-          <div class="w-full px-3">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+          <div className="w-full px-3">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
               Confirm Password
             </label>
             <input
-              class="appearance-none block w-full bg-gray-200  border-red-500 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-password"
+              className="appearance-none block w-full bg-gray-200  border-red-500 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               type="password"
               placeholder="******************"
             />
-            <p class="text-red-500 text-xs italic" data-aos="fade-in">
+            <p className="text-red-500 text-xs italic" data-aos="fade-in">
               Please fill out this field.
             </p>
           </div>
         </div>
-        <div class="flex flex-wrap -mx-3 mb-2 md:mb-0">
-          <div class="w-full px-3 mb-6 md:mb-5">
-            <label class="block uppercase tracking-wide border-red-500 text-xs font-bold mb-2">
+        <div className="flex flex-wrap -mx-3 mb-2 md:mb-0">
+          <div className="w-full px-3 mb-6 md:mb-5">
+            <label className="block uppercase tracking-wide border-red-500 text-xs font-bold mb-2">
               City
             </label>
             <input
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               type="text"
-              placeholder="Albuquerque"
+              placeholder="Lagos"
             />
-            <p class="text-red-500 text-xs italic" data-aos="fade-in">
+            <p className="text-red-500 text-xs italic" data-aos="fade-in">
               Please fill out this field.
             </p>
           </div>
-          <div class="w-full px-3 mb-4 md:mb-2">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+          <div className="w-full px-3 mb-4 md:mb-2">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
               Demo Number
             </label>
             <input
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               type="text"
               placeholder="+1234567890"
             />
-            <p class="text-red-500 text-xs italic" data-aos="fade-in">
+            <p className="text-red-500 text-xs italic" data-aos="fade-in">
               Please fill out this field.
             </p>
           </div>
@@ -105,7 +104,7 @@ const SignUp = () => {
         <div className="flex justify-center items-center">
           <button
             type="submit"
-            class="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 w-3/5 rounded-full h-10 mb-6 md:mb-1"
+            className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 w-3/5 rounded-full h-10 mb-6 md:mb-1"
           >
             Sign Up
           </button>
