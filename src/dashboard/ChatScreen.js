0f7cb4740501aa185f968bd4ec880ef89d03b2a0
ekenecf <ekens2u@gmail.com>
2023-03-22 15:36:35 +0100

    Add styles to components

--- src/dashboard/ChatScreen.js
+++ src/dashboard/ChatScreen.js
@@ -3,92 +3,120 @@ import image from '../images/nature.jpg'
 
 const ChatScreen = () => {
   return (
-   <div className='h-2/5 overflow-y-auto overscroll-y-auto mt-4 pb-1'>
-     <div className="mt-4 ml-4 flex mr-3 items-center">
-      <img
-        src={image}
-        className="rounded-full w-10 h-10 relative"
-        alt="userImg"
-      />
-      <div className="mr-20 ml-6">
-        <p className="text-base font-semibold text-white">Full Name of user</p>
-        <p className="text-sm text-slate-400">Last message of user</p>
+    <div className="h-2/5 overflow-y-auto overscroll-y-auto mt-4 " id='ChatScreenMainContainer'>
+      <div className="mt-4 flex justify-between mx-5 items-center">
+        <div className="flex justify-between gap-x-3">
+          <img
+            src={image}
+            className="rounded-full w-10 h-10 relative"
+            alt="userImg"
+          />
+          <div>
+            <p className="text-base font-semibold text-white">
+              Full Name of user
+            </p>
+            <p className="text-sm text-slate-400">Last message of user1</p>
+          </div>
+        </div>
+        <p className="text-slate-400">MsgTime</p>
       </div>
-      <p className="text-slate-400">MsgTime</p>
-    </div>
-     <div className="mt-4 ml-4 flex mr-3 items-center">
-      <img
-        src={image}
-        className="rounded-full w-10 h-10 relative"
-        alt="userImg"
-      />
-      <div className="mr-20 ml-6">
-        <p className="text-base font-semibold text-white">Full Name of user</p>
-        <p className="text-sm text-slate-400">Last message of user</p>
+      <div className="mt-4 flex justify-between mx-5 items-center">
+        <div className="flex justify-between gap-x-3">
+          <img
+            src={image}
+            className="rounded-full w-10 h-10 relative"
+            alt="userImg"
+          />
+          <div>
+            <p className="text-base font-semibold text-white">
+              Full Name of user
+            </p>
+            <p className="text-sm text-slate-400">Last message of user1</p>
+          </div>
+        </div>
+        <p className="text-slate-400">MsgTime</p>
       </div>
-      <p className="text-slate-400">MsgTime</p>
-    </div>
-     <div className="mt-4 ml-4 flex mr-3 items-center">
-      <img
-        src={image}
-        className="rounded-full w-10 h-10 relative"
-        alt="userImg"
-      />
-      <div className="mr-20 ml-6">
-        <p className="text-base font-semibold text-white">Full Name of user</p>
-        <p className="text-sm text-slate-400">Last message of user</p>
+      <div className="mt-4 flex justify-between mx-5 items-center">
+        <div className="flex justify-between gap-x-3">
+          <img
+            src={image}
+            className="rounded-full w-10 h-10 relative"
+            alt="userImg"
+          />
+          <div>
+            <p className="text-base font-semibold text-white">
+              Full Name of user
+            </p>
+            <p className="text-sm text-slate-400">Last message of user1</p>
+          </div>
+        </div>
+        <p className="text-slate-400">MsgTime</p>
       </div>
-      <p className="text-slate-400">MsgTime</p>
-    </div>
-     <div className="mt-4 ml-4 flex mr-3 items-center">
-      <img
-        src={image}
-        className="rounded-full w-10 h-10 relative"
-        alt="userImg"
-      />
-      <div className="mr-20 ml-6">
-        <p className="text-base font-semibold text-white">Full Name of user</p>
-        <p className="text-sm text-slate-400">Last message of user</p>
+      <div className="mt-4 flex justify-between mx-5 items-center">
+        <div className="flex justify-between gap-x-3">
+          <img
+            src={image}
+            className="rounded-full w-10 h-10 relative"
+            alt="userImg"
+          />
+          <div>
+            <p className="text-base font-semibold text-white">
+              Full Name of user
+            </p>
+            <p className="text-sm text-slate-400">Last message of user1</p>
+          </div>
+        </div>
+        <p className="text-slate-400">MsgTime</p>
       </div>
-      <p className="text-slate-400">MsgTime</p>
-    </div>
-     <div className="mt-4 ml-4 flex mr-3 items-center">
-      <img
-        src={image}
-        className="rounded-full w-10 h-10 relative"
-        alt="userImg"
-      />
-      <div className="mr-20 ml-6">
-        <p className="text-base font-semibold text-white">Full Name of user</p>
-        <p className="text-sm text-slate-400">Last message of user</p>
+      <div className="mt-4 flex justify-between mx-5 items-center">
+        <div className="flex justify-between gap-x-3">
+          <img
+            src={image}
+            className="rounded-full w-10 h-10 relative"
+            alt="userImg"
+          />
+          <div>
+            <p className="text-base font-semibold text-white">
+              Full Name of user
+            </p>
+            <p className="text-sm text-slate-400">Last message of user1</p>
+          </div>
+        </div>
+        <p className="text-slate-400">MsgTime</p>
       </div>
-      <p className="text-slate-400">MsgTime</p>
-    </div>
-     <div className="mt-4 ml-4 flex mr-3 items-center">
-      <img
-        src={image}
-        className="rounded-full w-10 h-10 relative"
-        alt="userImg"
-      />
-      <div className="mr-20 ml-6">
-        <p className="text-base font-semibold text-white">Full Name of user</p>
-        <p className="text-sm text-slate-400">Last message of user</p>
+      <div className="mt-4 flex justify-between mx-5 items-center">
+        <div className="flex justify-between gap-x-3">
+          <img
+            src={image}
+            className="rounded-full w-10 h-10 relative"
+            alt="userImg"
+          />
+          <div>
+            <p className="text-base font-semibold text-white">
+              Full Name of user
+            </p>
+            <p className="text-sm text-slate-400">Last message of user1</p>
+          </div>
+        </div>
+        <p className="text-slate-400">MsgTime</p>
       </div>
-      <p className="text-slate-400">MsgTime</p>
-    </div>
-     <div className="mt-4 ml-4 flex mr-3 items-center">
-      <img
-        src={image}
-        className="rounded-full w-10 h-10 relative"
-        alt="userImg"
-      />
-      <div className="mr-20 ml-6">
-        <p className="text-base font-semibold text-white">Full Name of user</p>
-        <p className="text-sm text-slate-400">Last message of user</p>
+      <div className="mt-4 flex justify-between mx-5 items-center">
+        <div className="flex justify-between gap-x-3">
+          <img
+            src={image}
+            className="rounded-full w-10 h-10 relative"
+            alt="userImg"
+          />
+          <div>
+            <p className="text-base font-semibold text-white">
+              Full Name of user
+            </p>
+            <p className="text-sm text-slate-400">Last message of user1</p>
+          </div>
+        </div>
+        <p className="text-slate-400">MsgTime</p>
       </div>
-      <p className="text-slate-400">MsgTime</p>
     </div>
-   </div>
   )
 }
 
--- src/dashboard/DashboardNav.js
+++ src/dashboard/DashboardNav.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { CgProfile } from 'react-icons/cg'
+import { BsFillChatDotsFill } from 'react-icons/bs'
+import { AiFillSetting } from 'react-icons/ai'
+import image from '../images/nature.jpg'
 
 const DashboardNav = () => {
   return (
-    <div>
-      Navigation
+    <div className="flex pt-4 mx-4 justify-between items-center">
+      <CgProfile className="h-5 w-5 text-slate-300" />
+      <BsFillChatDotsFill className="h-5 w-5 text-slate-300" />
+      <AiFillSetting className=" h-5 w-5 text-slate-300" />
+      <img src={image} className="rounded-full w-8 h-8 text-slate-300" alt="userImg" />
     </div>
   )
 }
